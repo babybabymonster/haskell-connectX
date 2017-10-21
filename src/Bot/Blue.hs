61e7376743930932ac223bc16e8568c12f25d0d8
Alice <alice@192-168-1-4.tpgi.com.au>
2017-10-21 14:26:46 +1100

    add minimax

--- src/Bot/Blue.hs
+++ src/Bot/Blue.hs
@@ -7,11 +7,188 @@
 module Bot.Blue where
     
 import Data.Board
+--import Data.Player
+import Data.List
+import Data.Ord
+import Data.Cell (Cell (Empty))
+import Data.Player
+import Data.Column
+import Data.List.Split (splitOn)
+import Data.Universe.Helpers (diagonals)
 
---data Tree Board = Node Board [Tree Board]
+data Tree a = Node{root :: a,
+                   subForest :: Forest a} deriving (Show,Eq)
+
+type Forest a = [Tree a]
+
+type GN = (Index, Player)
+
+type GNS = [(Index, Player)]
 
 makeMove :: Board -> LookAhead -> Int
-makeMove b _ = head $ filter (validMove b) [1..fst(dimension b)]
+makeMove b depth = case turn b of
+    BlueBot -> fst $ head $ snd $ maxAlg b (genTree b depth [])
+    RedBot -> fst $ head $ snd $ minAlg b (genTree b depth [])
+    _ -> error "no a valid player"
+
+-- generate a tree of [(Index, Player)]
+genTree :: Board -> LookAhead -> GNS -> Tree GNS
+genTree _ 0 gs = (Node gs [])
+genTree bod n gs = (Node gs $ map (genTree bod (n-1)) $ filter (\x -> validIndexes bod x) $ buildNodes gs (genGNS n bod))
+        -- where
+
+validIndexes :: Board -> GNS -> Bool
+validIndexes bb tuples
+    | snd (head (maxOfSecOfTuple tupleMovedIndexes)) <= (snd $ dimension bb) = True
+    | snd (head (maxOfSecOfTuple totalHeight)) <= (snd $ dimension bb) = True
+    | otherwise = error "something's wrong"
+    where
+            -- the height of each column which has been put piece on
+            totalHeight = zipWith (\x y -> (fst x, (snd x) + (snd y))) movedIndexesTuples tupleMovedIndexes
+            movedIndexesTuples = filter (\x -> (fst x) `elem` (map fst tupleMovedIndexes)) tupleColIndexes
+            tupleColIndexes = zip [1..fst $ dimension bb]lengthOfCol
+            lengthOfCol = map length (board bb)
+            -- get a list of tuples: (index, the occurence of this index)
+            tupleMovedIndexes = zip noDuplicates lengthOfIndexes
+            -- delete duplicate indexes in the list and sort the list
+            noDuplicates = map head $ group $ sort $ map fst tuples
+            -- get the occurence of each moved column indexes
+            lengthOfIndexes = map length $ group $ sort $ map fst tuples
+
+--             tupleMovedIndexes = zip noDuplicates lengthOfIndexes
+--             -- delete duplicate indexes in the list and sort the list
+--             noDuplicates = map head $ group $ sort listOfIndexes
+--             -- get the occurence of each moved column indexes
+--             lengthOfIndexes = map length groupedIndexes
+--             groupedIndexes = group $ sort listOfIndexes
+--             listOfIndexes = map fst tuples
+
+-- choose the tuple that has the largest second element
+maxOfSecOfTuple :: [(Index, Int)] -> [(Index, Int)]
+maxOfSecOfTuple tps = filter (\z -> snd z == bestScore) tps
+    where bestScore = maximum $ map snd tps
+
+
+-- mostCommon list = fst . maximumBy (compare `on` snd) $ elemCount
+--       where  elemCount = map (head &&& length) . group . sort $ list
+
+buildNodes :: GNS -> GNS -> [GNS]
+buildNodes gs1 gs2 = map (add gs1) gs2
+    where add gns g = gns ++ [g]
+
+genGNS :: LookAhead -> Board -> GNS
+genGNS look bd
+    | odd look = zip ordIndexes (replicate (length ordIndexes) (otherPlayer (turn bd)))
+    | otherwise = zip ordIndexes (replicate (length ordIndexes) (otherPlayer (turn bd)))
+    where   ordIndexes = sortBy (comparing (\i -> abs $ (fst(dimension bd) + 1) `div` 2 - i)) indexes
+            indexes = [1..fst(dimension bd)]
+
+evaluate :: Board -> Score
+evaluate bd = (myGetScore bd BlueBot) - (myGetScore bd (otherPlayer RedBot))
+
+maxAlg :: Board -> Tree GNS -> (Score, GNS)
+maxAlg maxb (Node x []) = (evaluate (myUpdateBoard maxb x), x)
+maxAlg maxb (Node _ ls) = maxOfTuple $ map (minAlg maxb) ls
+
+-- choose the tuple that has the largest first element
+maxOfTuple :: [(Score, GNS)] -> (Score, GNS)
+maxOfTuple tps = head $ filter(\z -> fst z == bestScore) tps
+    where bestScore = maximum $ map fst tps
+
+minAlg :: Board -> Tree GNS -> (Score, GNS)
+minAlg minb (Node x []) = (evaluate (myUpdateBoard minb x), x)
+minAlg minb (Node _ ls) = minOfTuple $ map (maxAlg minb) ls
+
+-- choose the tuple that has the smallest first element
+minOfTuple :: [(Score, GNS)] -> (Score, GNS)
+minOfTuple tps = head $ filter(\z -> fst z == bestScore) tps
+    where bestScore = minimum $ map fst tps
+
+myGetScore :: Board -> Player -> Score
+myGetScore b p = sum [columnScore, rowScore, diagonalScore, otherDiagScore]
+    where
+        streak    = connect b
+        minStreak = 1 + streak `div` 2
+        otherBot  = otherPlayer p
+
+        columnScore    =  calColScore $ filledMatrix
+        rowScore       =  calScore $ transpose $ filledMatrix
+        diagonalScore  =  calScore $ diagonals $ filledMatrix
+        otherDiagScore =  calScore $ diagonals $ map reverse $ filledMatrix
+
+        filledMatrix   = map (fillColumn Empty $ snd $ dimension b) $ board b
+
+        calColScore mat = sum $ (map (streakScore . length)) $ map concat
+                              $ map (splitOn [Empty]) $ filter ((>=streak).length)
+                                    $ map last $ map (splitOn [corresCell otherBot]) mat
+
+        calScore mat = sum $ (map (streakScore . length)) $ concatMap (splitOn [Empty])
+                           $ filter ((>=streak).length) $ map concat
+                                $ map (splitOn [corresCell otherBot]) mat
+
+--         calculateScore mat =
+--             sum $ (map (streakScore . length))
+--                 $ filter ((>=minStreak).length)
+--                     $ concatMap
+--                         (concatMap (splitOn [corresCell otherBot]))
+--                             $ map (splitOn [Empty]) mat
+
+        streakScore :: Int -> Score
+        streakScore i
+            | i < minStreak     = 0
+            | i < (streak -1)   = i * 60
+            | i == (streak -1)  = i * 100
+            | i >= streak       = 10000
+            | otherwise         = 0
+
+myUpdateBoard :: Board -> GNS -> Board
+myUpdateBoard grid (y : ys) = myUpdateBoard (updateBoardNoScore grid $ fst y) ys
+myUpdateBoard grid [] = grid
+
+
+--------------------------------------------------------
+
+
+
+--
+-- genDepthBoards :: Int -> Board -> [(Index, Board)]
+-- genDepthBoards 1 bb = genPossibleBoards bb
+-- genDepthBoards d bb = concatMap (genDepthBoards (d-1)) $ map snd (genPossibleBoards bb)
+--
+-- -- generate the next possible board situation
+-- genPossibleBoards :: Board -> [(Index, Board)]
+-- genPossibleBoards root = zip orderedIndexes (map (updateBoardNoScore root) orderedIndexes)
+--       where orderedIndexes = sortBy (comparing (\i -> abs $ (fst(dimension root) + 1) `div` 2 - i)) validIndexes
+--             validIndexes = filter (validMove root) [1..fst(dimension root)]
+--
+-- genOneLayer :: LookAhead -> Int -> Board -> Tree (Index, Board)
+-- genOneLayer 0 i r = (Node (i, r) [])
+-- genOneLayer d i r = (Node (i, r) (map (genOneLayer (d-1)) $ map (updateBoardNoScore r) orderedIndexes))
+--         where   -- order the index in a order that will prioritise the center columns.
+--                 orderedIndexes b = sortBy (comparing (\i -> abs $ (fst(dimension b) + 1) `div` 2 - i)) validIndexes
+--                 validIndexes b = filter (validMove b) [1..fst(dimension b)]
+
+
+-- | it works!
+-- genOneLayer :: LookAhead -> Board -> Tree Board
+-- genOneLayer 0 r = (Node r [])
+-- genOneLayer d r = (Node r (map (genOneLayer (d-1)) $ map (updateBoardNoScore r) orderedIndexes))
+--         where   -- order the index in a order that will prioritise the center columns.
+--                 orderedIndexes = sortBy (comparing (\i -> abs $ (fst(dimension r) + 1) `div` 2 - i)) validIndexes
+--                 validIndexes = filter (validMove r) [1..fst(dimension r)]
+
+-- treeMap :: (a -> b) -> RoseTree a -> RoseTree
+-- treeMap f (Node x []) = Node (f x) []
+-- treeMap f (Node x list) = Node (f x) map (treeMap f) list
+-- zip  $ map (updateBoardNoScore root) orderedIndexes
+
+--
+--
+
+
+
+
+-- generate the next possible board situation
 
 
 
@@ -23,33 +200,6 @@ makeMove b _ = head $ filter (validMove b) [1..fst(dimension b)]
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    -- Blue can win in 1 move.
-
-       -- or $ map hasWon $ map (updateBoard b) (validIndex b [1..maxIndex]) = 0
-       --where maxIndex = fst(dimension b) - 1
-    -- Red can win in 1 move, need to defend.
-    -- Minimax Choose the move with the highest score for Blue
-    -- and the lowest score for Red.
-
-
-
-------------------------------------------------
---getIndex :: Board -> [Int] -> [Int]
---getIndex b i = case i of
-  --  [] -> []
-   -- x : xs
- --       | or $ map (hasWon) (map (updateBoard b) (validIndex b i)) -> i
 
 -- we want the output [Int]
 --validIndex :: Board -> [Int] -> [Int]
--- src/Bot/Red.hs
+++ src/Bot/Red.hs
@@ -2,5 +2,25 @@ module Bot.Red where
 
 import Data.Board
 
+-- Define 'Move' type that contains the index of the column and
+-- the score of the board after inserting the piece into this column.
+type Move = (Index, Score)
+
+
 makeMove :: Board -> LookAhead -> Int
-makeMove b _ = head $ filter (validMove b) [1..fst(dimension b)]
+makeMove b _ = maxCorrIndex $ map (move b) validIndexes
+    where validIndexes = filter (validMove b) [1..fst(dimension b)]
+
+--max $ map (\ z -> getScore z (turn b)) possibleBoards
+  --  where possibleBoards = map (updateBoardNoScore b) (filter (validMove b) [1..fst(dimension b)])
+
+move :: Board -> Index -> Move
+move b i = (i, getScore (updateBoardNoScore b i) (turn b))
+
+-- find the maximum number of the second element of a list of tuples
+-- extract the corresponding first element of that tuple
+-- maybe more than one tuple have the bestScore, just choose the head
+-- of them.
+maxCorrIndex :: [(Index, Score)] -> Index
+maxCorrIndex lst = fst $ head $ filter (\ z -> snd z == bestScore) lst
+    where bestScore = maximum $ map snd lst
